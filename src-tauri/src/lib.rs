40e024c09b4f81c712c6ce9b56353a2f68983a07
Nicolás Baier Quezada <15371725+Debaq@users.noreply.github.com>
2026-03-04 15:17:15 -0300

    configuración para compilar para android

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,16 @@
 mod commands;
 mod storage;
 
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
-        .setup(|app| {
+        .setup(|_app| {
             #[cfg(target_os = "linux")]
             {
                 use tauri::Manager;
-                let main_window = app.get_webview_window("main").unwrap();
+                let main_window = _app.get_webview_window("main").unwrap();
                 main_window
                     .with_webview(|webview| {
                         use webkit2gtk::{PermissionRequestExt, WebViewExt};
